b3489bfcc8c4e95a0974a4b7bf795a38bd273d7d
ekils <bobobo746@hotmail.com>
2024-05-21 18:52:47 +0800

    update code

--- main.go
+++ main.go
@@ -134,7 +134,7 @@ func main() {
 		cronjobs.CronJobs()
 		// 計算下一次執行時間
 		// nextRun = nextRun.Add(time.Hour * 24)
-		nextRun = nextRun.Add(time.Minute * 1) //測試用
+		nextRun = nextRun.Add(time.Hour * 1) //測試用
 	}
 
 }
