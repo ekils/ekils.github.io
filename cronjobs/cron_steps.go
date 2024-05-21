de3eb9e3543f8e08791951c39ee8c99ba31605c8
ekils <bobobo746@hotmail.com>
2024-05-21 21:41:31 +0800

    test code

--- cronjobs/cron_steps.go
+++ cronjobs/cron_steps.go
@@ -1,6 +1,7 @@
 package cronjobs
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -24,58 +25,57 @@ func CronJobs() {
 	log.SetOutput(file)
 
 	// 測試用:
-	var AAPL []string
-	AAPL = append(AAPL, "AAPL")
-	Script(AAPL)
+	// var AAPL []string
+	// AAPL = append(AAPL, "AAPL")
+	// Script(AAPL)
 
-	/*
-		// Step1: 更新股價資訊 parse price history:
-		reuslt, companies, err := ParsePrice("NYSE", "STOCKHISTORY", "Company")
-		if err != nil {
-			log.Println(err)
-			fmt.Println(err)
-		}
-		log.Println(reuslt)
-		fmt.Println(reuslt)
+	// Step1: 更新股價資訊 parse price history:
+	reuslt, companies, err := ParsePrice("NYSE", "STOCKHISTORY", "Company")
+	if err != nil {
+		log.Println(err)
+		fmt.Println(err)
+	}
+	log.Println(reuslt)
+	fmt.Println(reuslt)
 
-		// Step2: 取得股價 parse price history:
-		reuslt_map_price, err := GetPrice_Historical("NYSE", "STOCKHISTORY", "Company", companies)
-		// log.Printf("reuslt_map_price: %v", reuslt_map_price)
-		if err != nil {
-			log.Println(err)
-			fmt.Println(err)
-		}
+	// Step2: 取得股價 parse price history:
+	reuslt_map_price, err := GetPrice_Historical("NYSE", "STOCKHISTORY", "Company", companies)
+	// log.Printf("reuslt_map_price: %v", reuslt_map_price)
+	if err != nil {
+		log.Println(err)
+		fmt.Println(err)
+	}
 
-		// // Step3: 取得eps parse eps history:(不用parse eps 是因為這是手動增加)
-		reuslt_map_eps, ok_companies, err := GetEPS_Historical("NYSE", "Company", companies)
-		log.Printf("reuslt_map_eps: %v", reuslt_map_eps)
-		fmt.Printf("reuslt_map_eps: %v", reuslt_map_eps)
+	// // Step3: 取得eps parse eps history:(不用parse eps 是因為這是手動增加)
+	reuslt_map_eps, ok_companies, err := GetEPS_Historical("NYSE", "Company", companies)
+	log.Printf("reuslt_map_eps: %v", reuslt_map_eps)
+	fmt.Printf("reuslt_map_eps: %v", reuslt_map_eps)
 
-		if err != nil {
-			log.Println(err)
-			fmt.Println(err)
-		}
+	if err != nil {
+		log.Println(err)
+		fmt.Println(err)
+	}
 
-		// Step3: gen pe-ratio report to db :
+	// Step3: gen pe-ratio report to db :
 
-		response, err = GenPE_Ratio(reuslt_map_price, reuslt_map_eps)
-		if err != nil {
-			log.Println(err)
-			fmt.Println(err)
-		} else {
-			log.Println(response)
-			fmt.Println(response)
-		}
-		// Step4: Gen plot link
-		// var companies = []string{"AAPL"}
+	response, err = GenPE_Ratio(reuslt_map_price, reuslt_map_eps)
+	if err != nil {
+		log.Println(err)
+		fmt.Println(err)
+	} else {
+		log.Println(response)
+		fmt.Println(response)
+	}
+	// Step4: Gen plot link
+	// var companies = []string{"AAPL"}
 
-		if len(ok_companies) > 0 {
-			Plot(ok_companies)
-			//Step5: Plot through Liff
-			Script(ok_companies)
-		} else {
-			log.Println("EPS Table都沒有資料, 請先Update")
-			fmt.Println("EPS Table都沒有資料, 請先Update")
-		}
-	*/
+	if len(ok_companies) > 0 {
+		Plot(ok_companies)
+		//Step5: Plot through Liff
+		Script(ok_companies)
+	} else {
+		log.Println("EPS Table都沒有資料, 請先Update")
+		fmt.Println("EPS Table都沒有資料, 請先Update")
+	}
+
 }
